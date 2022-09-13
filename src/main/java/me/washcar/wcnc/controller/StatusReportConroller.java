5fc9b662776016efe710dbbe63331e9a7d70c1fe
Gilteun Choi <gilteunchoi@gmail.com>
2022-09-14 02:32:25 +0900

    to use @RestController

--- src/main/java/me/washcar/wcnc/controller/StatusReportConroller.java
+++ src/main/java/me/washcar/wcnc/controller/StatusReportConroller.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import me.washcar.wcnc.Status.*;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StatusReportConroller {
 
     @GetMapping("ping")
-    @ResponseBody
     public Pong pingpong(){
         Pong pong = new Pong();
         pong.setMsg("pong");
