2beb3a131c2ff0c20b984eebf92126b1a7fe2e78
gilteun <gilteunchoi@gmail.com>
2022-10-23 03:07:45 +0900

    storeinfo bug fixed

--- src/main/java/me/washcar/wcnc/dto/StoreInfo.java
+++ src/main/java/me/washcar/wcnc/dto/StoreInfo.java
@@ -8,21 +8,26 @@ public class StoreInfo {
     //TODO 세차장리스트조회-Dto
     @Getter
     public static class searchStoreByLocationDto {
-        private List<String> store;
+        private List<Object> store;
     }
     //TODO 세차장정보-Dto
     @Getter
     public static class storeInfoDto {
-        private String store;
+        private String preview_image;
+        private List<String> images;
+        private String name;
     }
     //TODO 세차장메뉴-Dto
     @Getter
     public static class storeMenuDto {
-        private String store;
+        private List<Object> menu;
     }
     //TODO 세차장세부정보-Dto
     @Getter
     public static class storeDetailDto {
-        private String detail;
+        private String phone;
+        private String address;
+        private String wayto;
+        private String info;
     }
 }
