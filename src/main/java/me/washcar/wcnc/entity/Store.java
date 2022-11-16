8f8e34059c0791e151eb6cda74a691b1e874f2f2
shkisme <shkisme0130@gmail.com>
2022-11-17 01:22:06 +0900

    chore: 라인변경, 포메터 적용

--- src/main/java/me/washcar/wcnc/entity/Store.java
+++ src/main/java/me/washcar/wcnc/entity/Store.java
@@ -11,55 +11,56 @@ import java.util.List;
 @NoArgsConstructor
 public class Store {
 
-    @Builder
-    public Store(String name, String tel, String slug, String wayTo, String description, String previewImage, Boolean isApproved, Boolean isChecked) {
-        this.name = name;
-        this.tel = tel;
-        this.slug = slug;
-        this.wayTo = wayTo;
-        this.description = description;
-        this.previewImage = previewImage;
-        this.isApproved = isApproved;
-        this.isChecked = isChecked;
-    }
+  @Builder
+  public Store(String name, String tel, String slug, String wayTo, String description,
+      String previewImage, Boolean isApproved, Boolean isChecked) {
+    this.name = name;
+    this.tel = tel;
+    this.slug = slug;
+    this.wayTo = wayTo;
+    this.description = description;
+    this.previewImage = previewImage;
+    this.isApproved = isApproved;
+    this.isChecked = isChecked;
+  }
 
-    @Id
-    @GeneratedValue
-    private Long storeId;
-    @Column(nullable = false)
-    private String name;
+  @Id
+  @GeneratedValue
+  private Long storeId;
+  @Column(nullable = false)
+  private String name;
 
-    private String tel;
+  private String tel;
 
-    @Column(nullable = false, unique = true)
-    private String slug;
+  @Column(nullable = false, unique = true)
+  private String slug;
 
-    private String wayTo;
+  private String wayTo;
 
-    private String description;
+  private String description;
 
-    private String previewImage;
+  private String previewImage;
 
-    @Column(nullable = false)
-    private Boolean isApproved;
+  @Column(nullable = false)
+  private Boolean isApproved;
 
-    @Column(nullable = false)
-    private Boolean isChecked;
+  @Column(nullable = false)
+  private Boolean isChecked;
 
-    @OneToOne(mappedBy = "store")
-    private StoreLocation storeLocation;
+  @OneToOne(mappedBy = "store")
+  private StoreLocation storeLocation;
 
-    @OneToMany(mappedBy = "store")
-    private List<StoreImage> storeImages = new ArrayList<>();
+  @OneToMany(mappedBy = "store")
+  private List<StoreImage> storeImages = new ArrayList<>();
 
-    @OneToMany(mappedBy = "store")
-    private List<User_Store> user_stores = new ArrayList<>();
+  @OneToMany(mappedBy = "store")
+  private List<User_Store> user_stores = new ArrayList<>();
 
-    @OneToMany(mappedBy = "store")
-    private List<Reservation> reservations = new ArrayList<>();
+  @OneToMany(mappedBy = "store")
+  private List<Reservation> reservations = new ArrayList<>();
 
-    @OneToMany(mappedBy = "store")
-    private List<StoreOption> storeOptions = new ArrayList<>();
+  @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+  private List<StoreOption> storeOptions = new ArrayList<>();
 
 //    @OneToMany(mappedBy = "store")
 //    private List<Slot> slots = new ArrayList<>();
