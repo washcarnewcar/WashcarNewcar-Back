7b323dc5c50c58a09d26287f320854e34cd3ae30
shkisme <shkisme0130@gmail.com>
2022-11-17 01:23:08 +0900

    feat: 세차장 메뉴 get API 개발

--- src/main/java/me/washcar/wcnc/controller/StoreInfoController.java
+++ src/main/java/me/washcar/wcnc/controller/StoreInfoController.java
@@ -2,6 +2,9 @@ package me.washcar.wcnc.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.washcar.wcnc.dto.StoreInfo;
+import me.washcar.wcnc.dto.StoreInfo.MenuListResult;
+import me.washcar.wcnc.dto.StoreInfo.StoreInfoDto;
+import me.washcar.wcnc.dto.StoreInfo.StoreMenuDto;
 import me.washcar.wcnc.service.StoreInfoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,25 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class StoreInfoController {
-    private final StoreInfoService storeInfoService;
 
-    @GetMapping("/search/store")
-    public StoreInfo.searchStoreByLocationDto searchStoreByLocation(@RequestParam("longitude") float longitude, @RequestParam("latitude") float latitude){
-        return storeInfoService.searchStoreByLocation(longitude, latitude);
-    }
+  private final StoreInfoService storeInfoService;
 
-    @GetMapping("/store/{slug}/info")
-    public StoreInfo.storeInfoDto storeInfo(@PathVariable String slug) {
-        return storeInfoService.storeInfo(slug);
-    }
+  @GetMapping("/search/store")
+  public StoreInfo.searchStoreByLocationDto searchStoreByLocation(
+      @RequestParam("longitude") float longitude, @RequestParam("latitude") float latitude) {
+    return storeInfoService.searchStoreByLocation(longitude, latitude);
+  }
 
-    @GetMapping("/store/{slug}/menu")
-    public StoreInfo.storeMenuDto storeMenu(@PathVariable String slug) {
-        return storeInfoService.storeMenu(slug);
-    }
+  @GetMapping("/store/{slug}/info")
+  public StoreInfoDto getStoreInfo(@PathVariable String slug) {
+    return storeInfoService.getStoreInfo(slug);
+  }
 
-    @GetMapping("/store/{slug}/detail")
-    public StoreInfo.storeDetailDto storeDetail(@PathVariable String slug) {
-        return storeInfoService.storeDetail(slug);
-    }
+  @GetMapping("/store/{slug}/menu")
+  public MenuListResult<StoreMenuDto> storeMenu(@PathVariable String slug) {
+    return MenuListResult.getMenuListResult(storeInfoService.getStoreMenu(slug));
+  }
+
+  @GetMapping("/store/{slug}/detail")
+  public StoreInfo.storeDetailDto storeDetail(@PathVariable String slug) {
+    return storeInfoService.storeDetail(slug);
+  }
 }
