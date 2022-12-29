ed29556b7410ddd0895b6f4e5a6eb7941a09a460
gilteunchoi <gilteunchoi@gmail.com>
2022-12-29 22:12:49 +0900

    JWT에 ROLE 추가하는 코드 제거

--- src/main/java/me/washcar/wcnc/filter/CustomAuthenticationFilter.java
+++ src/main/java/me/washcar/wcnc/filter/CustomAuthenticationFilter.java
@@ -8,21 +8,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -47,16 +42,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException {
         User user = (User)authentication.getPrincipal();
-        Collection<GrantedAuthority> simpleGrantedAuthorityCollection = (Collection<GrantedAuthority>) user.getAuthorities();
 
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 180 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
-                .withClaim("roles", simpleGrantedAuthorityCollection.stream().toList().toString())
                 .sign(algorithm);
 
         String refresh_token = JWT.create()
