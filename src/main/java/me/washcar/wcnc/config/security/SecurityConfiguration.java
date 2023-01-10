74e969feb1da9038ad3b4eb7e23be1b48d4269c0
gilteunchoi <gilteunchoi@gmail.com>
2023-01-10 11:12:43 +0900

    rollback deprecated WebSecurityConfigurerAdapter

--- src/main/java/me/washcar/wcnc/config/security/SecurityConfiguration.java
+++ src/main/java/me/washcar/wcnc/config/security/SecurityConfiguration.java
@@ -4,58 +4,56 @@ import lombok.RequiredArgsConstructor;
 import me.washcar.wcnc.filter.CustomAuthenticationFilter;
 import me.washcar.wcnc.filter.CustomAuthorizationFilter;
 import me.washcar.wcnc.service.OAuth.CustomOAuth2UserService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@ConditionalOnDefaultWebSecurity
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class SecurityConfiguration {
-
-    private final AuthenticationEntryPointImpl authenticationEntryPoint;
-    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
-
-    @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationEntryPointImpl authenticationEntryPoint;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(
+                authenticationManagerBean());
 
         http.cors().and().csrf().disable();
         http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.oauth2Login()
-            .successHandler(oAuth2AuthenticationSuccessHandler)
-            .userInfoEndpoint()
-            .userService(customOAuth2UserService);
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
 
-        http.logout().disable();
+        http.authorizeRequests().antMatchers("/login", "/refresh/token", "/status/**").permitAll();
+        http.authorizeRequests().antMatchers("/super/**").hasAnyAuthority("ROLE_SUPER_ADMIN");
+        http.authorizeRequests().antMatchers("/provider/check-slug/*").permitAll();
+        http.authorizeRequests().antMatchers("/provider/**").hasAnyAuthority("ROLE_MANAGER");
+        http.authorizeRequests().antMatchers("/user").authenticated();
+        http.authorizeRequests().anyRequest().permitAll();
 
-        http.authorizeRequests(auth -> {
-            auth.antMatchers("/login", "/refresh/token", "/status/**").permitAll();
-            auth.antMatchers("/super/**").hasAnyAuthority("ROLE_SUPER_ADMIN");
-            auth.antMatchers("/provider/check-slug/*").permitAll();
-            auth.antMatchers("/provider/**").hasAnyAuthority("ROLE_USER");
-            auth.antMatchers("/user").authenticated();
-            auth.anyRequest().permitAll();
-        });
-
-        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-        http.addFilter(new CustomAuthenticationFilter(authenticationManager));
+        http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
     }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
